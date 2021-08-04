815ead115a53096ac4a4df6685f997f762c72400
geborenik <geborenik@gmail.com>
2021-08-04 23:53:23 +0900

    무한슬라이더 작업 시작

---
+++ src/components/infinitySlide.js
@@ -0,0 +1,10 @@
+import React, { Component, useEffect, useState } from 'react';
+
+export default class InfinitySlide extends Component {
+
+  const [slideNumber, setSlideNumberstate] = useState(1)
+
+  render() {
+    return <div></div>;
+  }
+}
